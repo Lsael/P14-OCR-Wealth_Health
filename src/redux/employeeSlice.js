8e5c21e361eaf56bf255893c85aedcdb477b6726
LSaelens <l.saelens@laposte.net>
2024-04-11 23:23:01 +0200

    Added Redux

---
+++ src/redux/employeeSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = []
+
+export const employeeSlice = createSlice({
+  name: "employee",
+  initialState,
+  reducers: {
+  }
+})
+
+export const {} = employeeSlice.actions
+
+export default employeeSlice.reducer
